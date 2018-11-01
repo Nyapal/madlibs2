40b944e39c62c5cda26d725cec918f9b8a938dc2
Nyapal Gatkuoth <nyapalgatkuoth@Nyapals-MacBook-Pro.local>
2018-10-31 17:15:39 -0700

    mad libs term2

---
+++ madlibs.py
@@ -0,0 +1,12 @@
+name = input('Welcome to MAD LIBS\n Enter your name: ')
+noun = input('Enter a noun: ')
+noun1 = input('Enter a noun: ')
+adj = input('Enter an adjective: ')
+adj1 = input('Enter an adjective: ')
+verb = input('Enter a verb: ')
+verb1 = input('Enter a verb: ')
+adj2 = input('Enter an adjective: ')
+
+story = adj1.upper() + " by: " + name + "\nThis " + noun + " got me feelin' pretty \nSo pardon if I'm impolite I just really need your " + noun1 + " with me \nI'm sorry 'bout the other night \nAnd I know I could be more creative \nAnd come up with " +  adj +  " lines \nBut I'm turnt up off sizz' and I love you \nIs the only thing that's in my mind \nYou take me " + adj1 + ", " + adj1 + " than I've ever been, babe \nJust come over, let's " + verb + ", babe \nI hope I ain't calling you too late, too late \nYou light my fire \nLet's stay up late and " + verb1 + " I wanna go back to the old way \nBut I'm " + adj2 + " and still with a full ash tray \nWith a little bit too much to say"
+
+print(story)
